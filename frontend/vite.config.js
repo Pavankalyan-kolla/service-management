aaa7dbca1c2907b6891716757eaed683de25cd32
Pavankalyan-kolla <pavankolla1020@gmail.com>
2025-09-30 11:20:24 +0530

    Ready for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000
-  }
+  base: '/', // Netlify handles routing, so root is fine
+  plugins: [react()]
 })
